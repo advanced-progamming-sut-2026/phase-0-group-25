21c460754af02f80fd60eb349fd18b2c65f67f22
Amir Khosravi <sam.khosravifard.85@sharif.edu>
2026-06-19 23:47:39 +0330

    another check on new branch

--- src/Menu/MenuManager.java
+++ src/Menu/MenuManager.java
@@ -1,10 +1,17 @@
 package src.Menu;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MenuManager {
+    private Scanner scanner = new Scanner(System.in);
     private Menu currentMenu;
     private ArrayList<Menu> availableMenus;
+
+    public MenuManager() {
+        availableMenus = new ArrayList<>();
+
+    }
 
     public void changeMenu(){
 
