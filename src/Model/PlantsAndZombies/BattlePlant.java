07e7bb9044715fcab07e256261b07ef43635e038
mshhamissi <mshhamissi86@gmail.com>
2026-06-20 19:57:56 +0430

    enhancing sun producers plants

--- src/Model/PlantsAndZombies/BattlePlant.java
+++ src/Model/PlantsAndZombies/BattlePlant.java
@@ -2,10 +2,12 @@ package src.Model.PlantsAndZombies;
 
 public class BattlePlant extends Plant {
     private double lastActionTime;
+    private double timeElapsedAsAlive;
     private PlantStats plantStats;
 
     public BattlePlant(PlantStats plantStats, String name) {
         this.lastActionTime = 0;
+        this.timeElapsedAsAlive = 0;
         this.plantStats = plantStats;
         this.name = name;
         this.currentHP = plantStats.getBaseHP();
@@ -23,5 +25,9 @@ public class BattlePlant extends Plant {
 
     public PlantStats getPlantStats() {
         return plantStats;
+    }
+
+    public double getTimeElapsedAsAlive() {
+        return timeElapsedAsAlive;
     }
 }
