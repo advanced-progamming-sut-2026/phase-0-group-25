e20a9d7b584ad0188ceda9e1cff36d812512471d
mshhamissi <mshhamissi86@gmail.com>
2026-07-03 23:34:09 +0430

    adding wall-nut plants ability

--- src/Model/PlantsAndZombies/Entity.java
+++ src/Model/PlantsAndZombies/Entity.java
@@ -5,9 +5,32 @@ public abstract class Entity {
     protected boolean isAlive;
     protected Position position;
 
+
     public abstract void update();
 
-    public boolean isAlive() {
-        return this.isAlive;
+
+    public void setCurrentHP(double currentHP) {
+        this.currentHP = currentHP;
+        if (this.currentHP <= 0) {
+            this.isAlive = false;
+        }
     }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public double getCurrentHP() {
+        return currentHP;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+
 }
