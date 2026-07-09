ff82db079b78ea78fddd530e03f9d99cfe634299
mshhamissi <mshhamissi86@gmail.com>
2026-07-09 13:55:56 +0430

    adding some primary zombie properties

--- src/Model/PlantsAndZombies/Zombie.java
+++ src/Model/PlantsAndZombies/Zombie.java
@@ -1,17 +1,66 @@
 package src.Model.PlantsAndZombies;
 
 import src.Model.PlantsAndZombies.Abilities.Ability;
+import src.Model.PlantsAndZombies.Armors.Armor;
 
 import java.util.*;
 
 public class Zombie extends Entity {
-    private String name;
+    private int currentHP;
     private ZombieStats zombieStats;
-    private double velocity;
+    private double currentVelocity;
     protected ArrayList<Ability> abilities;
+    private boolean isHalated;
+    private ArrayList<Armor> activeArmors;
+    //private HashMap<String, Double> effectsInfo;
+
+    public Zombie(ZombieStats zombieStats, Position position) {
+        this.zombieStats = zombieStats;
+        this.position = position;
+        this.currentHP = zombieStats.getBaseHP();
+        this.currentVelocity = zombieStats.getVelocity();
+        this.isHalated = false;
+
+        this.activeArmors = new ArrayList<>();
+        if (zombieStats.getArmors() != null) {
+            for (Armor armor : zombieStats.getArmors()) {
+                this.activeArmors.add(new Armor(armor.getType(),
+                        armor.getCurrentHP(), armor.isMetallic()));
+            }
+        }
+    }
+
 
     public void update() {
 
+    }
+
+    public void takeDamage(int damage) {
+        int leftoverDamage = damage;
+
+        for (Armor armor : activeArmors) {
+            leftoverDamage = armor.takeDamage(leftoverDamage);
+
+            if (armor.isDisarmed()) {
+                activeArmors.remove(armor);
+            }
+
+            if (leftoverDamage <= 0) {
+                break;
+            }
+        }
+
+        if (leftoverDamage > 0) {
+            this.currentHP -= leftoverDamage;
+            checkLife();
+        }
+    }
+
+    public void checkLife() {
+        if (this.currentHP <= 0) {
+            this.currentHP = 0;
+            this.isAlive = false;
+        }
     }
 
     public Position getPosition() {
--- src/Model/PlantsAndZombies/ZombieStats.java
+++ src/Model/PlantsAndZombies/ZombieStats.java
@@ -1,11 +1,34 @@
 package src.Model.PlantsAndZombies;
 
+import src.Model.PlantsAndZombies.Armors.Armor;
+
 import java.util.*;
 
 public class ZombieStats {
     private int baseHP;
-    private boolean isHalated;
-    private double damage;
-    private HashMap<String, Integer> armorsInfo;
-    private HashMap<String, Double> effectsInfo;
+    private double eatdps;
+    private double velocity;
+    private ArrayList<Armor> armors;
+    private int waveCost;
+
+
+    public int getBaseHP() {
+        return baseHP;
+    }
+
+    public double getEatdps() {
+        return eatdps;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public int getWaveCost() {
+        return waveCost;
+    }
+
+    public ArrayList<Armor> getArmors() {
+        return armors;
+    }
 }
