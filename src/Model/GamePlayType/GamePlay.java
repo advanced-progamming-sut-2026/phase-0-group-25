3262a145e28095e0b9ef2221755862124a466f02
SAH_JAFARI <s.a.jafari36@gmail.com>
2026-07-21 09:22:19 +0330

    some changes

--- src/Model/GamePlayType/GamePlay.java
+++ src/Model/GamePlayType/GamePlay.java
@@ -1,6 +1,7 @@
 package src.Model.GamePlayType;
 
 import src.Enums.ChapterType;
+import src.Model.Grave;
 import src.Model.Mower;
 import src.Model.PlantsAndZombies.*;
 import src.Model.PlantsAndZombies.Projectiles.Projectile;
@@ -22,6 +23,7 @@ public abstract class GamePlay {
     protected ArrayList<Zombie> gameZombies = new ArrayList<>();
     protected ArrayList<BattlePlant> gamePlants = new ArrayList<>();
 
+    protected ArrayList<Grave> allGraves = new ArrayList<>();
     protected ArrayList<Wave> allWaves = new ArrayList<>();
 
     protected int level;
@@ -42,7 +44,8 @@ public abstract class GamePlay {
     protected int numOfWaves;
     protected int totalTicksPassed = 0;
 
-    public GamePlay(ChapterType chapterType, int level, int difficulty, User thisUser) {
+    public GamePlay(ChapterType chapterType, int level, int difficulty, User thisUser,
+                            ArrayList<String> plants, ArrayList<String> zombies) {
         this.numOfPlantFood = 0;
         this.mySuns = 0;
         this.isPaused = false;
@@ -288,7 +291,7 @@ public abstract class GamePlay {
         System.out.println("===================");
 
         System.out.println("Board Legend:");
-        System.out.println("[ ] : Arable Tile     ~ ~ : Non-Arable Tile");
+        System.out.printf("[ ] : Arable Tile     ~ ~ : %s Tile\n", getKindOfTile(chapterType));
         System.out.println(" P  : Plant is present       Z  : Zombie is present");
         System.out.println(" M  : Mower (Ready)          X  : Mower (Used)");
         System.out.println("---------------------------------------------------------");
@@ -466,12 +469,56 @@ public abstract class GamePlay {
         }
     }
 
+    public ArrayList<Grave> getAllGraves() {
+        return allGraves;
+    }
+
+    public void setAllGraves(ArrayList<Grave> allGraves) {
+        this.allGraves = allGraves;
+    }
+
+    public void addGrave(Grave thisGrave) {
+        this.allGraves.add(thisGrave);
+    }
+
     public void glowingAward (GamePlay thisGame) {
         Boolean isGlowing = Math.random() <= 0.05;
         if (isGlowing) {
             thisGame.addPlantFood();
             System.out.printf("The glowing zombie dropped a plant food; you have %d plant foods now.\n",
                                     thisGame.getNumOfPlantFood());
+        }
+    }
+
+    public void updateZombieTiles() {
+        for (Tile tile : tiles) {
+            tile.getZombies().clear();
+        }
+
+        for (Zombie zombie : gameZombies) {
+            Position zombiePosition = Position.getRowAndColumn(zombie.getPosition());
+            int currentX = (int) zombiePosition.getX();
+            int currentY = (int) zombiePosition.getY();
+
+            Tile currentTile = tiles.stream()
+                    .filter(t -> (int) t.getPosition().getX() == currentX && (int) t.getPosition().getY() == currentY)
+                    .findFirst()
+                    .orElse(null);
+
+            if (currentTile != null) {
+                currentTile.getZombies().add(zombie);
+            }
+        }
+    }
+
+    public String getKindOfTile (ChapterType thisChapter) {
+        if (thisChapter == ChapterType.ANCIENT_EGYPT ||
+            thisChapter == ChapterType.DARK_AGE) {
+            return "Grave";
+        } else if (thisChapter == ChapterType.FROSTBITE_CAVES) {
+            return "Landslide";
+        } else {
+            return "Water";
         }
     }
 }
--- src/Model/GamePlayType/SaveOurSeeds.java
+++ src/Model/GamePlayType/SaveOurSeeds.java
@@ -1,13 +1,21 @@
 package src.Model.GamePlayType;
 
+import src.Enums.ChapterType;
+import src.Model.User.User;
+
 import java.util.ArrayList;
 
 public class SaveOurSeeds extends GamePlay {
-    private ArrayList<src.Model.PlantsAndZombies.BattlePlant> myPlants;
 
-    public ArrayList<src.Model.PlantsAndZombies.BattlePlant> getMyPlants() {
-        return myPlants;
+    public SaveOurSeeds(ChapterType chapterType, int level, int difficulty, User thisUser,
+                        ArrayList<String> plants, ArrayList<String> zombies) {
+        super(chapterType, level, difficulty, thisUser, plants, zombies);
     }
+
     @Override
-    public void update() {}
+    public void update() {
+        // TODO simple update
+
+
+    }
 }
