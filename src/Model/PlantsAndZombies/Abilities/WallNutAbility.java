2e3cb1ca7457a052d16736919700ad0826e24866
mshhamissi <mshhamissi86@gmail.com>
2026-07-31 18:50:13 +0430

    third debug

--- src/Model/PlantsAndZombies/Abilities/WallNutAbility.java
+++ src/Model/PlantsAndZombies/Abilities/WallNutAbility.java
@@ -48,9 +48,8 @@ public class WallNutAbility implements Ability {
                 int damage = checkEffected(plant);
 
                 for (Tile tile : GAME.getTiles()) {
-                    //todo: 1. getter of all zombies in proper range tile; 2. getter of row and column of tile
-                    int distanceX = Math.abs(tile.getRow() - plantRowAndColumn.getX());
-                    int distanceY = Math.abs(tile.getColumn() - plantRowAndColumn.getY());
+                    int distanceX = Math.abs((int) tile.getPosition().getX() - (int) plantRowAndColumn.getX());
+                    int distanceY = Math.abs((int) tile.getPosition().getY() - (int) plantRowAndColumn.getY());
                     if ((distanceX <= range) && (distanceY <= range)) {
                         for (Zombie zombie : tile.getZombies()) {
                             zombie.setCurrentHP(zombie.getCurrentHP() - damage);
